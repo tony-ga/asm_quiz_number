a97041a8f6f64d4a985463964b65f8b0f96b1b47
tony-ga <garcia1525.am@gmail.com>
2026-03-12 18:29:14 -0600

    refactor: mover número secreto de .data al stack
    Se eliminó la variable  de la sección  y el número secreto ahora
    se almacena en el stack durante la preparación del programa.
    
    Cambios principales:
    - Se quitó  de .
    - El número secreto ahora se inicializa con  seguido de .
    - En la rutina de comparación se recupera el valor usando .
    - Si la comparación falla, el número se vuelve a guardar en el stack con
      para mantenerlo disponible en el siguiente intento.
    
    Esto reduce el uso de memoria estática y mueve el estado del número secreto
    a memoria dinámica (stack) durante la ejecución.

--- quiz_number.asm
+++ quiz_number.asm
@@ -1,144 +1,80 @@
 ; --- ADIVINA EL NUMERO ENTERO ---
 
-
-
 section .data
-
 	out1 db "Adivina el numero entero secreto entre 0-9: "
-
 	out1_len equ $ - out1
 
-
-
 	out2 db "Adivinaste :)", 10
-
 	out2_len equ $ - out2
 
-
-
 	out3 db "Intentalo nuevamente: "
-
 	out3_len equ $ - out3
-
-
-
-	num db 6
-
-
 
 section .bss
 
 	user_input resb 8
 
-
-
 section .text
 
 	global _start
 
-
-
 _start:
-
-	mov rax, 1
-
-	mov rdi, 1
-
-	mov rsi, out1
-
-	mov rdx, out1_len
-
-	syscall
-
-
-
-	mov rax, 0
-
-	mov rdi, 0
-
-	mov rsi, user_input
-
-	mov rdx, 8
-
-	syscall
-
-	jmp thread
-
-
-
-try:
-
-
-
-	mov rax, 1
-
-	mov rdi, 1
-
-	mov rsi, out3
-
-	mov rdx, out3_len
-
-	syscall
-
-
-
-	mov rax, 0
-
-	mov rdi, 0
-
-	mov rsi, user_input
-
-	mov rdx, 8
-
-	syscall
-
-
-
-thread:
-
-
+	; 1. PREPARACION (Esconder el numero)
+	mov rax, 6	; El numero secreto es 6
+	push rax	; <--- LO GUARDAMOS EN EL STACK
 
 	xor rax, rax
 
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, out1
+	mov rdx, out1_len
+	syscall
 
+	mov rax, 0
+	mov rdi, 0
+	mov rsi, user_input
+	mov rdx, 8
+	syscall
+	jmp thread
 
-	movzx rax, byte [rsi]
+try:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, out3
+	mov rdx, out3_len
+	syscall
 
-	sub rax, 0x30
+	mov rax, 0
+	mov rdi, 0
+	mov rsi, user_input
+	mov rdx, 8
+	syscall
 
+thread:
+	; COMPARACION (RECUPERAR EL STACK)
+	movzx rbx, byte [user_input]
 
+	sub rbx, 0x30
 
-	mov rbx, rax
+	pop rax		; <--- SACAMOS EL 6 DEL STACK Y SE QUEDA EN RAX
 
-	cmp bl, [num]
-
-	jz correct ; SI bl - num = 0 SE CUMPLE LA CONDICION
-
+	cmp bl, al
+	jz correct ; SI bl - al = 0 SE CUMPLE LA CONDICION
+	push rax
 	jmp try
-
-
-
 
 correct:
 
 	mov rax, 1
-
 	mov rdi, 1
-
 	mov rsi, out2
-
 	mov rdx, out2_len
-
 	syscall
-
 	jmp end
-
-
 
 end:
 
 	mov rax, 60
-
 	mov rdi, 0
-
 	syscall
